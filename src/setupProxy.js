7da21f0b9dbb0e72be37c00b129d48b2384083b2
wksmirnowa <merriel@inbox.ru>
2020-12-24 23:38:13 +0300

    fixed setup paths

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,11 +2,11 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(
-        '/api',
+        '/api/',
         createProxyMiddleware({
             target: 'http://localhost:5000',
             pathRewrite: {
-                '^/api': '',
+                '^/api/': '',
             },
         })
     );
