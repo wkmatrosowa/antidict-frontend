537dc7e3a7ea49c00ee307ac7ef34d4eeb3e7e27
wksmirnowa <merriel@inbox.ru>
2021-03-23 12:41:02 +0300

    add setWordsByCategory

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -41,6 +41,7 @@ export function Main() {
                 setSearchWords(arrWords);
                 setHighlightedText(text);
                 setCategories(arrCategories);
+                setWordsByCategory(wordsByCategory);
             });
     }
 
